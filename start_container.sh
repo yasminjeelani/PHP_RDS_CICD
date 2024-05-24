2ba3909d303eacb51d38e184d5b1d0a6dee0d07f
yasminjeelani <36299748+yasminjeelani@users.noreply.github.com>
2024-05-24 14:36:56 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+$(aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 150878085644.dkr.ecr.ap-south-1.amazonaws.com)
 # Pull the Docker image from ecr repo
 docker pull 150878085644.dkr.ecr.ap-south-1.amazonaws.com/demo:latest
 # Run the Docker image as a container
