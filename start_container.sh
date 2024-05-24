4f189a458b6477e95ea09321d8c198ea4c40242b
yasminjeelani <36299748+yasminjeelani@users.noreply.github.com>
2024-05-24 14:56:37 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 150878085644.dkr.ecr.ap-south-1.amazonaws.com
-# Pull the Docker image from ecr repo
-docker pull 150878085644.dkr.ecr.ap-south-1.amazonaws.com/demo:latest
-# Run the Docker image as a container
-docker run -d --name myphpcontainer -p 8080:80 150878085644.dkr.ecr.ap-south-1.amazonaws.com/demo:latest
+aws ecr get-login-password --region <AWS_REGION> | docker login --username AWS --password-stdin <AWS_ACCOUNT_ID>.dkr.ecr.<AWS_REGION>.amazonaws.com# Pull the Docker image from ecr repo
+docker pull <YOUR_IMAGE_NAME>:<TAG># Run the Docker image as a container
+docker run -d --name myphpcontainer -p 8080:80 <YOUR_IMAGE_NAME>:<TAG>
