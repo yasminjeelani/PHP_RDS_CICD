214748d3ce56d0c1d45ed0588e98394e6f7e8aab
yasminjeelani <36299748+yasminjeelani@users.noreply.github.com>
2024-05-24 14:02:12 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull 150878085644.dkr.ecr.ap-south-1.amazonaws.com/demo
 
 # Run the Docker image as a container
-docker run -d --name myphpcontainer -p 8080:80 public.ecr.aws/r1g5u8w6/demo
+docker run -d --name myphpcontainer -p 8080:80 150878085644.dkr.ecr.ap-south-1.amazonaws.com/demo
